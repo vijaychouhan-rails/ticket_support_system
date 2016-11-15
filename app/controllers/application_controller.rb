143ae2e8821badbfcac06eb390fe412409ff21e4
Vijay <vijaychouhan.rails@gmail.com>
2016-11-15 13:14:35 +0530

    handle not matched routes and record not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,23 @@ class ApplicationController < ActionController::Base
   # To permit extra params of user
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActionController::RoutingError, :with => :not_found
+
+  # Called by last route matching unmatched routes.  Raises RoutingError which will be rescued from in the same way as other exceptions.
+  def raise_not_found!
+    raise ActionController::RoutingError.new("No route matches #{params[:unmatched_route]}")
+  end
+
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_type])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :user_type])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+
+  private
+    def not_found
+      render json: { status: 'fail', message: 'The page you are looking for does not exist' } and return
     end
 end
